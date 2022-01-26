dda5d2dbd87ee60f9a6c4024f9dc9812ab8051ee
Vince Li <chickentaco823@gmail.com>
2022-01-26 01:02:17 -0500

    Add files via upload

---
+++ ShopSystem/Item.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Item : MonoBehaviour
+{
+    //public prefab variables
+    public GameObject displayImage;
+    public TextMeshProUGUI priceText;
+
+    //item attributes
+    //int price;
+    int ID;
+    //string itemName;
+    //string image;
+    //string description;
+    //bool bought;
+
+    void Awake(){
+        GameObject items = GameObject.Find("Panel");
+        GameObject shop = GameObject.Find("ContInfo");
+        ID = items.GetComponent<Items>().getID();
+        /*price = shop.GetComponent<OneTimeItemsInfo>().getPrice(ID);
+        itemName = shop.GetComponent<OneTimeItemsInfo>().getName(ID);
+        image = shop.GetComponent<OneTimeItemsInfo>().getImage(ID);
+        description = shop.GetComponent<OneTimeItemsInfo>().getDescription(ID);
+        bought = shop.GetComponent<OneTimeItemsInfo>().isBought(ID);*/
+        displayImage.GetComponent<Image>().sprite = Resources.Load<Sprite>(shop.GetComponent<OneTimeItemsInfo>().getImage(ID));
+        priceText.text = shop.GetComponent<OneTimeItemsInfo>().getPrice(ID).ToString();
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public void click(){
+        GameObject shop = GameObject.Find("ContInfo");
+        GameObject displayItem = GameObject.Find("CurrentItem");
+        GameObject displayDescription = GameObject.Find("CurrentDescription");
+        GameObject displayPrice = GameObject.Find("CurrentPrice");
+        displayItem.GetComponent<DisplayCurItem>().setID(ID);
+        displayDescription.GetComponent<DisplayCurDescription>().setID(ID);
+        displayPrice.GetComponent<DisplayCurPrice>().setID(ID);
+    }
+
+    /*public int getPrice(){
+        return price;
+    }
+
+    /*public string getName(){
+        return itemName;
+    }
+
+    public string getImage(){
+        return image;
+    }
+
+    public string getDescription(){
+        return description;
+    }
+
+    public bool isBought(){
+        return bought;
+    }*/
+
+}
